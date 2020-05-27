468ff7e26722b83929c66073fb63d23262e0e6df
rohit <rohit@appzui.com>
2020-05-28 02:34:24 +0530

    initial commit

--- min_stack.java
+++ min_stack.java
@@ -1,47 +1,45 @@
-class GfG
-{
+import java.util.*;
+
+class GfG {
     int minEle = -1;
     Stack<Integer> s = new Stack<Integer>();
 
-    int getMin()
-    {
-	   return minEle;
+    int getMin() {
+        return minEle;
     }
-    
-    int pop()
-    {
-	    if(!s.empty()){
+
+    int pop() {
+        if (!s.empty()) {
             int top = s.pop();
-            if(s.empty()){
-                minEle = -1; 
+            if (s.empty()) {
+                minEle = -1;
                 return top;
             }
-            
-            if(top < minEle){
+
+            if (top < minEle) {
                 int previous = minEle;
                 minEle = minEle - top;
                 return previous;
-                
+
             }
-        	return top;
-	    }
-	    
-	    return -1;
+            return top;
+        }
+
+        return -1;
     }
 
-    void push(int x)
-    {
-	    if(s.empty()){
-	        s.push(x);
-	        minEle = x;
-	    } else {
-	        if(x < minEle){
+    void push(int x) {
+        if (s.empty()) {
+            s.push(x);
+            minEle = x;
+        } else {
+            if (x < minEle) {
                 s.push(x - minEle);
                 minEle = x;
-                
+
             } else {
                 s.push(x);
             }
-	    }
-    }	
+        }
+    }
 }
--- warehousing.java
+++ warehousing.java
@@ -1,34 +1,34 @@
+
 //import for Scanner and other utility classes
 import java.util.*;
 
 class TestClass {
-    public static void main(String args[] ) throws Exception {
+    public static void main(String args[]) throws Exception {
 
-        //Scanner
+        // Scanner
         Scanner s = new Scanner(System.in);
-        Hashtable<String, Integer> h = new Hashtable<String, Integer>(); 
+        Hashtable<String, Integer> h = new Hashtable<String, Integer>();
         String[] units = s.nextLine().split(",");
         int n = units.length;
         String[][] arr = new String[units.length - 1][4];
 
-        for(int i = 0; i < n - 1; i++){
+        for (int i = 0; i < n - 1; i++) {
             String[] values = s.nextLine().split(" ");
             arr[i][0] = "1";
-            arr[i][1] = values[0]; //unit
-            arr[i][2] = values[2]; //coeff
-            arr[i][3] = values[3]; //unit
+            arr[i][1] = values[0]; // unit
+            arr[i][2] = values[2]; // coeff
+            arr[i][3] = values[3]; // unit
         }
 
-        //initialize with LCM
-        for(int j = 0; j < n; j++){
+        // initialize with LCM
+        for (int j = 0; j < n; j++) {
             int ans = 1;
-            for(int i = 0; i < n - 1; i++){
+            for (int i = 0; i < n - 1; i++) {
                 int coeff1 = Integer.parseInt(arr[i][0]);
                 int coeff2 = Integer.parseInt(arr[i][2]);
-                if(units[j].equals(arr[i][1])){
+                if (units[j].equals(arr[i][1])) {
                     ans = lcm(ans, coeff1);
-                }
-                else if(units[j].equals(arr[i][3])){
+                } else if (units[j].equals(arr[i][3])) {
                     ans = lcm(ans, coeff2);
                 }
             }
@@ -36,20 +36,20 @@ class TestClass {
         }
 
         boolean flag = true;
-        while(flag){
+        while (flag) {
 
             flag = false;
-            for(int j = 0; j < n - 1; j++){
+            for (int j = 0; j < n - 1; j++) {
                 int lcm1 = h.get(arr[j][1]);
                 int lcm2 = h.get(arr[j][3]);
 
                 int value1 = Integer.parseInt(arr[j][0]);
                 int value2 = Integer.parseInt(arr[j][2]);
 
-                int multiplier1 = lcm1/value1;
-                int multiplier2 = lcm2/value2;
+                int multiplier1 = lcm1 / value1;
+                int multiplier2 = lcm2 / value2;
 
-                if(multiplier1 * multiplier2 > 1)
+                if (multiplier1 * multiplier2 > 1)
                     flag = true;
 
                 value1 = value1 * multiplier1 * multiplier2;
@@ -62,42 +62,39 @@ class TestClass {
                 h.put(arr[j][3], value2);
             }
         }
-       
 
         List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(h.entrySet());
- 
-        //sort the entries based on the value by custom Comparator
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
-        public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
-            return entry1.getValue().compareTo( entry2.getValue() );
-        }
+
+        // sort the entries based on the value by custom Comparator
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue().compareTo(entry2.getValue());
+            }
         });
- 
+
         Map<String, Integer> mapSortedByValues = new LinkedHashMap<String, Integer>();
-        
+
         String[] result = new String[n];
         int k = 0;
-        //put all sorted entries in LinkedHashMap
-        for( Map.Entry<String, Integer> entry : list  ){
+        // put all sorted entries in LinkedHashMap
+        for (Map.Entry<String, Integer> entry : list) {
             // mapSortedByValues.put(entry.getKey(), entry.getValue());
             result[k++] = entry.getValue() + entry.getKey();
         }
-        
+
         System.out.println(String.join(" = ", result));
     }
 
-    static int gcd(int a, int b) 
-    { 
-        if (a == 0) 
-            return b;  
-        return gcd(b % a, a);  
-    } 
-      
-    // method to return LCM of two numbers 
-    static int lcm(int a, int b) 
-    { 
-        if(a < b)
-            return (a*b)/gcd(a, b);
-        return (a*b)/gcd(b, a); 
-    } 
+    static int gcd(int a, int b) {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    // method to return LCM of two numbers
+    static int lcm(int a, int b) {
+        if (a < b)
+            return (a * b) / gcd(a, b);
+        return (a * b) / gcd(b, a);
+    }
 }
